683b7644da4e19ef37ebfc75a778335ad5f46aa8
chiedza1234 <cmutapati@gmail.com>
2023-02-18 01:58:54 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point to program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the 
+ * standard error
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
