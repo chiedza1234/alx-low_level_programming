4db9a328ab462391987fc8fa6729cfb9a65baaf3
chiedza1234 <cmutapati@gmail.com>
2023-02-21 14:08:12 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+*main -print alphabet in lowercase
+*return void
+*/
+
+void print_alphabet(void)
+{
+char c;
+for (c='a';c<='z';c++)
+{
+_putchar(c);
+}
+_putchar ('\n');
+}
